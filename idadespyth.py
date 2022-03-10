aca05431929a8a8ac46901c99b745605206cceb8
Daniel Alecrim <98345886+danielalecrimofc@users.noreply.github.com>
2022-03-10 11:08:58 -0300

    Implementação dias entre datas sem o ano bissexto

--- idadespyth.py
+++ idadespyth.py
@@ -1,5 +1,9 @@
+'''Anos divisíveis pelo número 4 são considerados bissextos.
+Anos divisíveis por 100 não são bissextos.
+Anos divisíveis por 400 são bissextos.'''
+
 '''com base em duas datas digitadas pelo usuário informe a quantidade de dias entre elas'''
-diasm = [31,28,31,30,31,30,30,31,31,30,31,30,31]
+diasm = [31,28,31,30,31,30,31,31,30,31,30,31]
 
 
 diai = int(input("Digite o dia da data inicial:"))
@@ -10,26 +14,46 @@ diac = int(input("Digite o dia da data chegada:"))
 mesc = int(input("Digite o mês da data chegada:"))
 anoc = int(input("Digite o ano da data chegada:"))
 
+diaia = diai
+mesia = mesi
+anoia = anoi
+
+diacb = diac
+mescb = mesc
+anocb = anoc
+
+diaf = diac
+mesf = 1
 diast = 0
 contdiasm = 0
 while anoi != anoc:
     diasdomesat = diasm[mesi-1]
-    while diai < diasdomesat:
+    for diai in range(diai,diasdomesat):
         diast += 1
         if diai == diasdomesat - 1:
             diai = 0
 
+
     mesi += 1
-    if mesi == 13 :
+
+    if mesi == 13:
         mesi = 1
         anoi += 1
 
-    if diai == diac and mesi == mesc and anoi == anoc:
-        break
 
-print(diast)
+    if anoi == anoc:
+        while mesf < mesc:
+            diasdta = diasm[mesf - 1]
+            for diai in range(diai,diasdta):
+                diast += 1
+                if diai == diasdta - 1:
+                    diai = 0
+            mesf += 1
+            if mesf == mesc:
+                diai = 1
+                for diai in range(diai,diaf + 1):
+                    diast += 1
 
 
+print(f"Da data {diaia}/{mesia}/{anoia} até {diacb}/{mescb}/{anocb} passaram-se {diast} dias !")
 
-'''07/03/1990
-12/07/1990'''
